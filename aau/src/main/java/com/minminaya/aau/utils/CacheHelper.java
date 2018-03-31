425addb8311697bb17b21ec3d50ad3d230d4ed4a
minminaya <minminaya@gmail.com>
2018-03-31 23:28:11 +0800

    缓存帮助类

--- aau/src/main/java/com/minminaya/aau/utils/CacheHelper.java
+++ aau/src/main/java/com/minminaya/aau/utils/CacheHelper.java
@@ -1,9 +1,19 @@
 package com.minminaya.aau.utils;
 
+import android.util.ArrayMap;
+
 /**
  * <p>Created by LGM on 2018-03-31 23:15</p>
  * <p>Email:minminaya@gmail.com</p>
  */
 public class CacheHelper {
-    
+
+    /**这里设计成全局单例*/
+    public static void getINSTANCE() {
+    }
+
+    /**设计为Builder模式，方便参数的设置*/
+    public class Builder {
+
+    }
 }
