6f8a65acaabb371fa234340ff2bb8778399bdde4
minminaya <minminaya@gmail.com>
2018-04-30 00:00:03 +0800

    MODIFIED:modified CacheHelper

--- aau/src/main/java/com/minminaya/aau/utils/CacheHelper.java
+++ aau/src/main/java/com/minminaya/aau/utils/CacheHelper.java
@@ -1,5 +1,6 @@
 package com.minminaya.aau.utils;
 
+import android.app.Notification;
 import android.util.ArrayMap;
 
 /**
@@ -7,6 +8,10 @@ import android.util.ArrayMap;
  * <p>Email:minminaya@gmail.com</p>
  */
 public class CacheHelper {
+
+    private String mCacheName;
+    private long mMaxSize;
+    private long mMaxCount;
 
     private static volatile CacheHelper defaultInstance;
 
@@ -33,7 +38,14 @@ public class CacheHelper {
      * 设计为Builder模式，方便参数的设置
      */
     public class CacheHelperBuilder {
+        public CacheHelperBuilder() {
 
+        }
+
+        public CacheHelperBuilder setCacheName(String cacheName) {
+            defaultInstance.mCacheName = cacheName;
+            return this;
+        }
     }
 
     /**
