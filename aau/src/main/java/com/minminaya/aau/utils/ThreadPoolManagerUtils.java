9c91cb552b3fbaf9e33fdb7d6f29c401c30d9fc8
lgm <lgm@meitu.com>
2018-07-03 22:14:26 +0800

    添加线程池工具类的线程池常量

--- aau/src/main/java/com/minminaya/aau/utils/ThreadPoolManagerUtils.java
+++ aau/src/main/java/com/minminaya/aau/utils/ThreadPoolManagerUtils.java
@@ -25,6 +25,13 @@ import java.util.concurrent.TimeUnit;
  * @time Created by 2018/6/29 13:51
  */
 public class ThreadPoolManagerUtils {
+
+    //几个代表线程池的静态常量
+    public final static String THREAD_POOL_ONE = "thread_pool_one";
+    public final static String THREAD_POOL_TWO = "thread_pool_two";
+    public final static String THREAD_POOL_THREE = "thread_pool_three";
+    public final static String THREAD_POOL_FOUR = "thread_pool_four";
+
     //确保该类只有一个实例对象
     private static ThreadPoolManagerUtils sInstance;
     //线程池集合
@@ -67,7 +74,7 @@ public class ThreadPoolManagerUtils {
      * 使用线程池，线程池中线程的创建完全是由线程池自己来维护的，我们不需要创建任何的线程
      * <p>通过指定的线程池来执行线程</p>
      *
-     * @param threadPoolKey 指定线程池的名字
+     * @param threadPoolKey 指定线程池的名字 ，如{@link ThreadPoolManagerUtils#THREAD_POOL_ONE,ThreadPoolManagerUtils#THREAD_POOL_TWO,ThreadPoolManagerUtils#THREAD_POOL_THREE,ThreadPoolManagerUtils#THREAD_POOL_FOUR}
      * @param runnable      在线程池里面运行的线程
      */
     public void executeRunnableBySpeciallyPool(String threadPoolKey, Runnable runnable) {
